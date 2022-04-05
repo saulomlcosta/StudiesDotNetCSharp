59e106de5e823258d92b266444dbbfd2f068d328
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-04-05 13:41:25 -0300

    Adding Interfaces

---
+++ CQRSMediaTrFrancisTest/CQRSMediaTrFrancisTest/Interfaces/ICommandHandlers/IMakeOrderCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using CQRSMediaTrFrancisTest.RequestModels.CommandRequestModels;
+using CQRSMediaTrFrancisTest.ResponseModels.CommandResponseModels;
+
+namespace CQRSMediaTrFrancisTest.Interfaces.ICommandHandlers
+{
+    public interface IMakeOrderCommandHandler
+    {
+        MakeOrderResponseModel MakeOrder(MakeOrderRequestModel requestModel);
+    }
+}
---
+++ CQRSMediaTrFrancisTest/CQRSMediaTrFrancisTest/Interfaces/IQueryHandlers/IGetOrderByIdQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRSMediaTrFrancisTest.RequestModels.QueryRequestModels;
+using CQRSMediaTrFrancisTest.ResponseModels.QueryResponseModels;
+
+namespace CQRSMediaTrFrancisTest.Interfaces.IQueryHandlers
+{
+    public interface IGetOrderByIdQueryHandler
+    {
+        GetOrderByIdResponseModel GetOrderById(GetOrderByIdRequestModel requestModel);
+    }
+}
