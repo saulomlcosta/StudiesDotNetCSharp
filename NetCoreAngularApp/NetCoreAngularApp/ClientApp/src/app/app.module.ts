c5b2ede38549c2fbb669544bf6ab88ccaa8cb135
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-06-15 15:37:35 -0300

    Remove not essential pages

--- NetCoreAngularApp/NetCoreAngularApp/ClientApp/src/app/app.module.ts
+++ NetCoreAngularApp/NetCoreAngularApp/ClientApp/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./nav-menu/nav-menu.component";
-// import { HomeComponent } from "./home/home.component";
 import { UsersComponent } from "./users/users.component";
 import { UserDataService } from "./_data-services/user.data-service";
 import { Interceptor } from "./app.interceptor.module";
@@ -17,10 +16,7 @@ import { Interceptor } from "./app.interceptor.module";
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      // { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "users", component: UsersComponent },
-    ]),
+    RouterModule.forRoot([{ path: "", component: UsersComponent }]),
     Interceptor,
   ],
   providers: [UserDataService],
