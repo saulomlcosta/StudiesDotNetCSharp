1756b54506038ef582a4e6408a1dfb060261c8b0
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-12-15 22:39:54 -0300

    Minimum SignalR Client and Server

---
+++ MinSignalRServCli/MiniSignalClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var uri = "http://localhost:5271/chat";
+
+await using var connection = new HubConnectionBuilder().WithUrl(uri).Build();
+
+await connection.StartAsync();
+
+await foreach (var date in connection.StreamAsync<DateTime>("Streaming"))
+{
+    Console.WriteLine(date);
+}
